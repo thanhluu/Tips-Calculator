825f9ae06c138f5b8c46a5f8b23d7d37342f1a64
thanhluu <thanhlt90@gmail.com>
2016-07-02 16:45:38 +0700

    bill amount is always the first responder

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -24,6 +24,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         tipLabel.text = "$0.00"
         totalLabel.text = "$0.00"
+        self.billField.becomeFirstResponder()
     }
 
     override func viewWillAppear(animated: Bool) {
